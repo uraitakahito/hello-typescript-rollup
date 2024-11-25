921c8e1d796e587b037b69261e45f8e4f15a2596
Takahito Urai <uraitakahito@gmail.com>
2024-11-25 18:51:29 +0900

    Refactor Rollup configuration to use RollupOptions type for improved type safety

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
-// https://rollupjs.org/command-line-interface/#config-intellisense
-import { defineConfig } from "rollup";
+import type { RollupOptions } from "rollup";
 
-const config = defineConfig({
+const config: RollupOptions = {
   input: "./src/app.ts", // conditionally required
   output: [
     {
       file: "bundle.js",
       format: "iife",
-    }
+    },
   ],
   plugins: [typescript()],
-});
+};
 
 export default config;
