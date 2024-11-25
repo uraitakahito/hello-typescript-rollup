07c4bd2e636573af236ff45f68c51742f51930bd
Takahito Urai <uraitakahito@gmail.com>
2024-11-25 16:23:50 +0900

    Refactor Rollup configuration to use defineConfig for improved IntelliSense support

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import typescript from "@rollup/plugin-typescript";
+// https://rollupjs.org/command-line-interface/#config-intellisense
+import { defineConfig } from "rollup";
 
-export default {
+const config = defineConfig({
   input: "./src/app.ts",
   output: {
     file: "bundle.js",
     format: "iife",
   },
   plugins: [typescript()],
-};
+});
+
+export default config;
