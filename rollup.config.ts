d0d68f4fc020a54b94a5b902c2d6edd37b022748
Takahito Urai <uraitakahito@gmail.com>
2024-12-27 07:00:51 +0900

    Add @uraitakahito/hello-npmjs dependency and implement suppress-warning module

--- rollup.config.ts
+++ rollup.config.ts
@@ -41,6 +41,23 @@ const config: RollupOptions[] = [
     ],
     plugins: [typescript({ rootDir: 'src' })],
   },
+  {
+    input: './src/import-check/suppress-warning.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'es',
+        // Make sure to remove the sourceMap option from your tsconfig.json:
+        // https://github.com/rollup/plugins/issues/216#issuecomment-1776899097
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript({ rootDir: 'src' })],
+
+    // The hello-npmjs package is not bundled into the output.
+    // https://rollupjs.org/troubleshooting/#warning-treating-module-as-external-dependency
+    external: ['@uraitakahito/hello-npmjs'],
+  },
 ];
 
 export default config;
---
+++ src/import-check/suppress-warning.ts
@@ -0,0 +1,3 @@
+import hello from '@uraitakahito/hello-npmjs';
+
+console.log(hello.say());
