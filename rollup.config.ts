ba060b946574cc45897dc9aa7f9cd599eb922dcd
Takahito Urai <uraitakahito@gmail.com>
2025-01-11 13:01:05 +0900

    Add import-external-commonjs and import-conditional-exports modules; update package.json with new dependencies and rollup.config.ts for conditional exports handling

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,10 @@
+/* eslint-disable max-len */
 import typescript from '@rollup/plugin-typescript';
 import type { RollupOptions } from 'rollup';
 // The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules.
 import resolve from '@rollup/plugin-node-resolve';
+// To generate a minified bundle with terser
+import terser from '@rollup/plugin-terser';
 
 const config: RollupOptions[] = [
   {
@@ -29,6 +32,55 @@ const config: RollupOptions[] = [
       },
     ],
     plugins: [typescript({ rootDir: 'src' })],
+  },
+
+  {
+    input: 'src/import-check/import-conditional-exports.ts',
+
+    output: [
+      {
+        dir: 'dist/iife',
+        format: 'iife',
+        plugins: [terser()],
+      },
+      {
+        dir: 'dist/es6',
+        format: 'es',
+      },
+      {
+        dir: 'dist/umd',
+        format: 'umd',
+      },
+    ],
+    plugins: [
+      resolve(),
+    ],
+
+    //
+    // Sample configuration to explicitly throw an error if an external dependency is not found.
+    // By default, Rollup only shows a warning and the build succeeds if an external dependency is not found.
+    // https://rollupjs.org/configuration-options/#onwarn
+    //
+    onwarn(warning, warn) {
+      if (warning.code === 'UNRESOLVED_IMPORT') {
+        throw new Error(`Unresolved import: ${warning}`);
+      }
+      warn(warning);
+    },
+  },
+
+  {
+    input: './src/import-check/import-conditional-exports.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'es',
+        // Make sure to remove the sourceMap option from your tsconfig.json:
+        // https://github.com/rollup/plugins/issues/216#issuecomment-1776899097
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript({ rootDir: 'src' }), resolve()],
   },
   {
     input: './src/import-check/import-external-esmodule.ts',
