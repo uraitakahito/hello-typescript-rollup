d3fe466aeb91b0967f021508efc3fe655b1ed6eb
Takahito Urai <uraitakahito@gmail.com>
2025-01-30 12:33:32 +0900

    Add root element to index.html and update main.ts to use it for canvas creation

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import randomSquare, {
   draw, reportArea, reportPerimeter,
 } from './modules/square.js';
 
-const myCanvas = create('myCanvas', document.body, 480, 320);
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('Failed to get root element');
+}
+
+const myCanvas = create('myCanvas', root, 480, 320);
 const reportList = createReportList(myCanvas.id);
 
 if (!myCanvas.ctx) {
